e840290d4505cc6ea2d435327c3a08e3b9a499f3
UlisesJP29 <jiaupjp@gmail.com>
2024-06-25 12:20:57 -0600

    completamos el proyecto 1

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,31 @@
+import {  Component, EventEmitter, Input, input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: `./list.component.html`,
+  styleUrl: './list.component.css',
+})
+export class ListComponent {
+
+  @Input()
+  public characterList: Character[] = [{
+    name: 'Trunks',
+    power: 3000
+  }];
+
+  // onDelete =
+
+
+  @Output()
+  onDelete: EventEmitter<string> = new EventEmitter();
+
+
+  onDeleteCharacter(id?:string):void {
+    console.log(id);
+    if (!id) {
+      return;
+    }
+    this.onDelete.emit( id );
+  }
+ }
